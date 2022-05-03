cc2dd595de9340cb312132c7b24cbbbf79cd205a
Sarah S <mrs.sarahso@gmail.com>
2022-05-03 19:23:34 +1000

    updated CSS, app, html files, login

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+from logging.handlers import RotatingFileHandler
 from time import sleep
 from venv import create
 import psycopg2
 from flask import Flask, render_template, request, redirect, session
 import os
+import bcrypt
+
 
 DATABASE_URL = os.environ.get('DATABASE_URL', 'dbname=project2db')
 SECRET_KEY = os.environ.get('SECRET_KEY', 'pretend secret key for testing')
@@ -10,13 +13,15 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'pretend secret key for testing')
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 
+@app.route('/moodlog')
+def moodlog():
+     # connection variable to cnnect to database
+    
+    DATABASE_URL=os.environ.get('HEROKU_POSTGRESQL_CRIMSON_URL','dbname = moodtracker')
 
-@app.route('/')
-def index():
-    # connection variable to cnnect to database
-    conn = psycopg2.connect('dbname = moodtracker')
+    conn = psycopg2.connect(DATABASE_URL)
     cur = conn.cursor()
-    cur.execute('SELECT id, name, mood_rating, diet_rating, sleep_rating, created_on::timestamp FROM moods_diet_sleep;')
+    cur.execute('SELECT id, name, mood_rating, diet_rating, sleep_rating, created_on::timestamp FROM moods_diet_sleep ORDER BY created_on DESC;')
     results = cur.fetchall()
 
     names = []
@@ -38,7 +43,13 @@ def index():
 
     length = len(names)
 
-    return render_template('index.html', names = names, mood_rating = mood_rating, diet_rating = diet_rating, sleep_rating= sleep_rating, length = length, created_on = created_on, username = session.get('username'))
+    return render_template('moodlog.html', names = names, mood_rating = mood_rating, diet_rating = diet_rating, sleep_rating= sleep_rating, length = length, created_on = created_on,username = session.get('username'))
+
+@app.route('/')
+def index():
+   
+
+    return render_template('index.html',  username = session.get('username'))
 
 
 @app.route('/add_mood_items',  methods=['POST'])
@@ -49,7 +60,9 @@ def add_mood_items():
     diet_rating = request.form.get("diet_rating")
     sleep_rating = request.form.get("sleep_rating")
    
-    conn = psycopg2.connect('dbname = moodtracker')
+    DATABASE_URL=os.environ.get('HEROKU_POSTGRESQL_CRIMSON_URL','dbname = moodtracker')
+
+    conn = psycopg2.connect(DATABASE_URL)
     cur = conn.cursor()
     cur.execute('INSERT INTO moods_diet_sleep(name, mood_rating, diet_rating, sleep_rating) VALUES(%s,%s, %s, %s)', [name, mood_rating, diet_rating, sleep_rating])
 
@@ -67,36 +80,32 @@ def login():
 
 @app.route('/login', methods=['POST'])
 def login_action():
-    username = request.form.get('username')
+
+    email= request.form.get('email')
     password = request.form.get('password')
     
+
 
     emails_fromsql = []
     usernames_fromsql = []
 
-    conn = psycopg2.connect('dbname = moodtracker')
+    
+    DATABASE_URL=os.environ.get('HEROKU_POSTGRESQL_CRIMSON_URL','dbname = moodtracker')
+
+    conn = psycopg2.connect(DATABASE_URL)
     cur = conn.cursor()
-    cur.execute('SELECT username, email, password_hash FROM users;')
+    cur.execute('SELECT username, email, password_hash FROM users WHERE email = %s', [email])
+
     results_fetch = cur.fetchall()
 
-    for column in results_fetch: 
+    print(results_fetch)
 
-        emails_fromsql.append(column[1])
-        usernames_fromsql.append(column[0])
-   
-    print(emails_fromsql)
-
-    # username = usernames_fromsql
-
-    cur.execute(f"SELECT name FROM users WHERE email like '{email}' ;")
-    username = cur.fetchone()
-
-    print(username[0])
 
     if email in emails_fromsql:
 
         session['email'] = email
-        session['username'] = username[0]
+        session['username'] = username
+        
 
         # Check if this is a valid email (in the database)
         # If valid - set the user ID in session and redirect
@@ -109,8 +118,25 @@ def login_action():
 
         return redirect('/login', ) 
 
+@app.route('/sign_up', methods = ['POST'])
+def signup():
 
+    username = request.form.get('signup_username')
+    email = request.form.get('signup_email')
+    password = request.form.get('signup_password')
 
+    password_hashed = bcrypt.hashpw(password.encode(),bcrypt.gensalt()).decode()
+
+    DATABASE_URL=os.environ.get('HEROKU_POSTGRESQL_CRIMSON_URL','dbname = moodtracker')
+
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+
+    cur.execute('INSERT INTO users (username, email, password_hash VALUES (%s, %s, %s', [username, email, password_hashed])
+
+    conn.commit()
+
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- schema.sql
+++ schema.sql
@@ -4,12 +4,7 @@ DROP TABLE IF EXISTS diet;
 DROP TABLE IF EXISTS moods;
 
 
-CREATE TABLE users (
-user_id SERIAL PRIMARY KEY,
-username VARCHAR(255),
-email VARCHAR(255),
-password_hash VARCHAR(255)
-);
+
 
 CREATE TABLE moods_diet_sleep (
 id SERIAL PRIMARY KEY,
@@ -18,6 +13,13 @@ mood_rating INTEGER,
 diet_rating INTEGER,
 sleep_rating INTEGER, 
 created_on TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE users (
+user_id SERIAL PRIMARY KEY,
+username VARCHAR(255),
+email VARCHAR(255),
+password_hash VARCHAR(255)
 );
 
 CREATE TABLE diet (
@@ -37,7 +39,7 @@ emotions VARCHAR(255)
 INSERT INTO moods_diet_sleep (name, mood_rating, sleep_rating, diet_rating) VALUES('Sarah',5, 6, 4);
 
 
-INSERT INTO users (email, name, password_hash) VALUES ('bob@example.com', 'Bob Example', '$2b$12$TpvX8HcLDBfx.LOaNefsI.hBFars/nSddrxqcYDxb5VDQfughklBa')
-INSERT INTO users (email, name, password_hash) VALUES ('sally@example.com', 'Sally Example', '$2b$12$d8iYmpmksEHmPqnCVDxGC.ZDID3KkS6jR8uH.fHewhOYm7UXJaJv6')
-INSERT INTO users (email, name, password_hash) VALUES ('sarah@example.com', 'Sarah S', '$2b$12$9g26teeYwsnDNQg.5D4EAOhT9uWneeAid.O1ppRSQLJVeUcZAUVPC' )
-INSERT INTO users (email, name, password_hash) VALUES ('jenny@example.com', 'Jenny J', '$2b$12$9g26teeYwsnDNQg.5D4EAOhT9uWneeAid.O1ppRSQLJVeUcZAUVPC')
+INSERT INTO users (email, username, password_hash) VALUES ('bob@example.com', 'BobExample', '$2b$12$TpvX8HcLDBfx.LOaNefsI.hBFars/nSddrxqcYDxb5VDQfughklBa');
+INSERT INTO users (email, username, password_hash) VALUES ('sally@example.com', 'SallyExample', '$2b$12$d8iYmpmksEHmPqnCVDxGC.ZDID3KkS6jR8uH.fHewhOYm7UXJaJv6');
+INSERT INTO users (email, username, password_hash) VALUES ('sarah@example.com', 'SarahS', '$2b$12$9g26teeYwsnDNQg.5D4EAOhT9uWneeAid.O1ppRSQLJVeUcZAUVPC' );
+INSERT INTO users (email, username, password_hash) VALUES ('jenny@example.com', 'JennyJ', '$2b$12$G1jYAe3jBCJyQQ74HnvL0enP6KkWRfMUZRoLRpBbPLwlmN.qKru6a');
